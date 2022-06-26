bc8416027a0fea001367422d9baf65fdb64dc394
AndrewJBateman <gomezbateman@yahoo.com>
2022-06-26 17:05:36 +0200

    refactor: use async Observable in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,31 @@
-import { Component } from '@angular/core';
-import { NewsApiService } from './news-api.service';
-
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+import { NewsApiService } from "./news-api.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-
   // declare empty arrays for articles and news sources
-  mArticles: Array<any>;
-  mSources: Array<any>;
+  mArticles$: Observable<any>;
+  mSources$: Observable<any>;
 
   constructor(private newsapi: NewsApiService) {
-    console.log('app component constructor called');
+    console.log("app component constructor called");
   }
 
-// tslint:disable-next-line: use-life-cycle-interface
+  // tslint:disable-next-line: use-life-cycle-interface
   ngOnInit() {
-    // load articles
-    this.newsapi.initArticles().subscribe(data => this.mArticles = data['articles']);
-    // load news sources
-    this.newsapi.initSources().subscribe(data => this.mSources = data['sources']);
-    }
+    this.mArticles$ = this.newsapi.initArticles();
+    this.mSources$ = this.newsapi.initSources();
+  }
+
 
   // function to search for articles based on a news source (selected from UI mat-menu)
   searchArticles(source) {
-    console.log('selected source is: ' + source);
-    this.newsapi.getArticlesByID(source).subscribe(data => this.mArticles = data['articles']);
+    console.log("selected source is: " + source);
+    this.mArticles$ = this.newsapi.getArticlesByID(source);
   }
-
 }
